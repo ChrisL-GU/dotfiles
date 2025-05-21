6862c22c7ff6446b579c6ce28486e94bffd32976
Christopher Lopes <lopes@gonzaga.edu>
2025-05-21 14:49:56 -0700

    Add LESS setting and `.bin` to path

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -7,11 +7,18 @@ if not contains $HOME/bin $PATH
     set -gx PATH $HOME/bin $PATH
 end
 
+# F: Automatically quit if content fits in one screen
+# R: Show raw control characters (e.g., color codes)
+# X: Don’t clear the screen on exit
+export LESS=FRX
+
 set -x EDITOR "nvim"
 set -x VISUAL "nvim"
 
-set -x NPM_INSTALL "/home/clopes/.npm-global"
+set -x NPM_INSTALL "$HOME/.npm-global"
 set -gx PATH $PATH $NPM_INSTALL
+
+set -gx PATH $PATH "$HOME/.bin"
 
 alias ..="cd .."
 alias ...="cd ../.."
